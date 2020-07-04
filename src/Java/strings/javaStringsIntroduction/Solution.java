04cdf9753d72213b8f4d024fd5192d55064bcb4d
nazmul <nazmul.avanto@gmail.com>
2020-07-05 01:44:06 +0200

    feat: done java string

---
+++ src/Java/strings/javaStringsIntroduction/Solution.java
@@ -0,0 +1,28 @@
+package Java.strings.javaStringsIntroduction;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        sc.nextLine();
+        String B=sc.next();
+        System.out.println(A.length() + B.length());
+        if(A.compareTo(B)> 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+        System.out.printf("%s %s", capitalizeFirst(A), capitalizeFirst(B));
+        /* Enter your code here. Print output to STDOUT. */
+
+    }
+
+    private static String capitalizeFirst(String name) {
+        return name.substring(0,1).toUpperCase() + name.substring(1);
+    }
+}
