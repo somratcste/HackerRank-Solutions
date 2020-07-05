d73167219cb8f4d2e863579ce1a41cc8924583c5
nazmul <nazmul.avanto@gmail.com>
2020-07-05 02:05:35 +0200

    feat: done java introduction

---
+++ src/Java/introduction/javaEndOfFile/Solution.java
@@ -0,0 +1,17 @@
+package Java.introduction.javaEndOfFile;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        int index = 1;
+        while(scanner.hasNext()) {
+            System.out.printf("%d %s\n", index++, scanner.nextLine());
+        }
+        scanner.close();
+    }
+}
