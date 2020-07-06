2d4632a11a65f551f41b42bae534b178da957cdb
nazmul <nazmul.avanto@gmail.com>
2020-07-06 14:56:04 +0200

    feat: exception handling

---
+++ src/Java/exceptionHandling/javaTryCatch/Solution.java
@@ -0,0 +1,26 @@
+package Java.exceptionHandling.javaTryCatch;
+
+import jdk.internal.util.xml.impl.Input;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        try {
+            Scanner scan = new Scanner(System.in);
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            scan.close();
+            int division;
+            division = a/b;
+            System.out.println(division);
+        } catch (InputMismatchException e) {
+            System.out.println(e.getClass().getName());
+        } catch (ArithmeticException e) {
+            System.out.println("java.lang.ArithmeticException: / by zero");
+        }
+    }
+}
